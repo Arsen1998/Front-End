bdf0c4d784b5b5e391ee2a5b17ac83a9644ed636
Arsen1998 <baghdasaryanarsen16@gmail.com>
2020-10-23 20:50:33 +0400

    ars

---
+++ Javascript/lesson-2-data-types/lesson/index.js
@@ -0,0 +1,20 @@
+/**
+ * JS data types
+ * (Tvyalneri tiper)
+ */
+
+ //1 Primitive types
+ //2 number (...,NaN,-Infinity)
+ //3 string
+ //4 boolean (true,false)
+ //5 undefined
+ // 6null
+ //+-
+
+
+ var a = 10;
+ var b = 230;
+
+ document.write("Hello World")
+ 
+
