77b983714367e79c4d01d56844f02877a524f329
Arsen1998 <baghdasaryanarsen16@gmail.com>
2020-10-29 03:13:27 +0400

    ars

--- Javascript/lesson-2-data-types/lesson/index.js
+++ Javascript/lesson-2-data-types/lesson/index.js
@@ -1,20 +1,39 @@
 /**
  * JS data types
- * (Tvyalneri tiper)
+ * (տվյալների տիպերը)
  */
-
- //1 Primitive types
- //2 number (...,NaN,-Infinity)
- //3 string
- //4 boolean (true,false)
- //5 undefined
- // 6null
- //+-
-
-
- var a = 10;
- var b = 230;
-
- document.write("Hello World")
- 
+// ծրագրավորման լեզուները լինում են թույլ կամ հաստատուն տիպավորված
+// Dynamic(թույլ) & Static(հաստատուն)
+// JavaScript ... Dynamic Typed language (թույլ տիպավորված լեզու)
+// C++, Java ... Static Typed language (հաստատուն տիպավորված լեզու)
+// Python ... Dynamic/Static Typed language (կիսաթույլ / կիսահաստատուն տիպավորված լեզու)
+// Primitive types / հասարակ տիպեր
+// 1 number (..., NaN, Infinity, -Infinity)
+// 2 string
+// 3 boolean (true, false)
+// 4 undefined
+// 5 null
+// +-
+// Reference types / հղումային տիպեր․․․ կանրադառնանք հետագայում
+/**
+ * Operators
+ */
+ // 1. unar (+, -)
+ // 2. binar (+, -, *, /, %), (=, +=, -=, *=, /=, %= , ... , (նշան=)), (>, <, >=, <=, ==, !=, ===, !==), (||, &&), (|, &)
+ // 3. ternar
+ var x = 10;
+ x = x + 10; // x += 10; 
+ x = x - 25; // x -= 25; 
+ x = x * 4; // x *= 4; 
+ // ...
+ // && 
+ // (true && true) = true
+ // (true && false) = false
+ // (false && true) = false
+ // (false && false) = false
+ // || 
+ // (true || true) = true
+ // (true || false) = true
+ // (false || true) = true
+ // (false || false) = false
 
