70a43038e0af0a9a8fe29f2484192982208c5d48
Arsen1998 <baghdasaryanarsen16@gmail.com>
2021-01-05 16:50:24 +0400

    ars

---
+++ Javascript/Tan@ nor das/app.js
@@ -0,0 +1,44 @@
+
+window.addEventListener("scroll", checkScroll);
+document.addEventListener("DOMContentLoaded", checkScroll);
+
+function checkScroll() {
+    let scrollPos = window.scrollY;
+
+    if(scrollPos > 0) {
+        header.classList.add('red');
+    } else {
+        header.classList.remove('red');
+    }
+}
+
+function currentTime() {
+    var date = new Date(); /* creating object of Date class */
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+    hour = updateTime(hour);
+    min = updateTime(min);
+    sec = updateTime(sec);
+    document.getElementById("clock").innerText = hour + " : " + min + " : " + sec; /* adding time to the div */
+      var t = setTimeout(function(){ currentTime() }, 1000); /* setting timer */
+  }
+  
+  function updateTime(k) {
+    if (k < 10) {
+      return "0" + k;
+    }
+    else {
+      return k;
+    }
+  }
+  
+  currentTime(); /* calling currentTime() function to initiate the process */
+
+
+
+
+
+
+
+
